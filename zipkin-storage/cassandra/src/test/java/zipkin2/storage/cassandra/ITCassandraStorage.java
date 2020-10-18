1b3961f0cc24d73a9f8a936889455678d06e5c55
adriancole <adrian.f.cole@gmail.com>
2020-10-18 16:39:21 +0800

    whoops?

--- zipkin-storage/cassandra/src/test/java/zipkin2/storage/cassandra/ITCassandraStorage.java
+++ zipkin-storage/cassandra/src/test/java/zipkin2/storage/cassandra/ITCassandraStorage.java
@@ -47,7 +47,7 @@ import static zipkin2.storage.cassandra.InternalForTests.writeDependencyLinks;
 class ITCassandraStorage {
 
   @RegisterExtension CassandraStorageExtension backend = new CassandraStorageExtension(
-    "openzipkin/zipkin-cassandra:test");
+    "openzipkin/zipkin-cassandra:2.21.7");
 
   @Nested
   class ITTraces extends zipkin2.storage.ITTraces<CassandraStorage> {
